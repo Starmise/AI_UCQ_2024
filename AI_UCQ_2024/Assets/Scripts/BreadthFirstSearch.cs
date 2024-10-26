a792b0d0ebd135ce2d7c7dc24fef857356f04af5
Luis R <78448904+Starmise@users.noreply.github.com>
2024-10-26 17:38:59 -0600

    BFS comprobation added
    (Help, I dond't know how, I only know it works)

--- AI_UCQ_2024/Assets/Scripts/BreadthFirstSearch.cs
+++ AI_UCQ_2024/Assets/Scripts/BreadthFirstSearch.cs
@@ -79,4 +79,81 @@ public class BreadthFirstSearch : MonoBehaviour
 
         return neighbors;
     }
+
+
+    public HashSet<Edge> EdgeSet = new HashSet<Edge>();
+    void Start()
+    {
+        // Caso 1: Exitoso (Hay camino)
+        Debug.Log("Caso 1: Exitoso");
+        Node H = new Node("H");
+        Node E = new Node("E");
+        Node A = new Node("A");
+        Node B = new Node("B");
+        Node C = new Node("C");
+        Node D = new Node("D");
+        Node F = new Node("F");
+
+        // Ahora queremos declarar las aristas.
+        Edge EdgeHE = new Edge("HE", H, E);
+        Edge EdgeEA = new Edge("EA", E, A);
+        Edge EdgeEF = new Edge("EF", E, F);
+        Edge EdgeAB = new Edge("AB", A, B);
+        Edge EdgeBC = new Edge("BC", B, C);
+        Edge EdgeBD = new Edge("BD", B, D);
+
+        EdgeSet.Add(EdgeHE);
+        EdgeSet.Add(EdgeEA);
+        EdgeSet.Add(EdgeEF);
+        EdgeSet.Add(EdgeAB);
+        EdgeSet.Add(EdgeBC);
+        EdgeSet.Add(EdgeBD);
+
+        List<Node> path;
+        bool found = BFS(H, D, out path);
+
+        if (found)
+        {
+            Debug.Log("¡Se encontró un camino! Camino:");
+            foreach (Node node in path)
+            {
+                Debug.Log(node.Name);
+            }
+        }
+        else
+        {
+            Debug.Log("No se encontró camino.");
+        }
+
+        // Caso 2: Fallido (No hay camino)
+
+        Debug.Log("----------------------------------------");
+        Debug.Log("Caso 2: Fallido");
+        Node G = new Node("G");
+
+        Edge Edge2HE = new Edge("HE", H, E);
+        Edge Edge2EF = new Edge("EF", E, F);
+        Edge Edge2EG = new Edge("EA", E, G);
+
+        EdgeSet.Add(Edge2HE);
+        EdgeSet.Add(Edge2EF);
+        EdgeSet.Add(Edge2EG);
+        // D queda desconectado
+
+        found = BFS(H, D, out path);
+
+        if (found)
+        {
+            Debug.Log("¡Se encontró un camino! Camino:");
+            foreach (Node node in path)
+            {
+                Debug.Log(node.Name);
+            }
+        }
+        else
+        {
+            Debug.Log("No se encontró camino.");
+        }
+    }
+
 }
